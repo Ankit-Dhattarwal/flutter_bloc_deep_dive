1e21dad87751b5e6f31d9e8d2f1e6b84572bb0ad
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2025-02-03 00:22:45 +0530

    login form validation basic-setup

---
+++ lib/login-form-validation/widgets/gradient_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_deep_dive_bloc_with_project/app_color.dart';
+
+class GradientButton extends StatelessWidget {
+  const GradientButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            AppColor.gradient1,
+            AppColor.gradient2,
+            AppColor.gradient3,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: const Text(
+          'Sign in',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 17,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_deep_dive_bloc_with_project/todo/add_todos.dart';
-import 'package:flutter_deep_dive_bloc_with_project/todo/todo_cubits/todo_cubits.dart';
-import 'package:flutter_deep_dive_bloc_with_project/todo/todo_list.dart';
+import 'package:flutter_deep_dive_bloc_with_project/login-form-validation/login_screen.dart';
+
+import 'app_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => TodoCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (_) => const TodoList(),
-          '/add-todo': (_) => const AddTodoPage(),
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColor.backgroundColor,
       ),
+      home: const LoginScreen(),
     );
   }
 }

885ea98f86eac79d42633ef14e9171cd1274e191
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2025-02-03 23:26:29 +0530

    show data on home page with help of watch method in bloc but this is not good approach

--- lib/login-form-validation/widgets/home_screen.dart
+++ lib/login-form-validation/widgets/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_deep_dive_bloc_with_project/bloc/counter_bloc.dart';
+import 'package:flutter_deep_dive_bloc_with_project/login-form-validation/bloc/auth_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /*
+    But remember that by this,
+     it rebuild when changes in AuthBloC
+      so avoid this use the AuthBuilder.
+     */
+  final authState = context.watch<AuthBloc>().state as AuthSuccess;
     return Scaffold(
       appBar: AppBar(),
-      body: Center(child: Text('Yes'),),
+      body: Center(
+        child: Text(authState.ui),
+      ),
     );
   }
 }
